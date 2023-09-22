878a5a2f7ecb9f4d5a8d8ff7b3e6ae877cb80f2f
sergiopmdev <sergio.pm.dev@proton.me>
2023-09-22 16:33:50 +0000

    [SOK-01] Implement home and not-found views
    Implementation of home and not-found view routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
+import HomeViewVue from '@/views/HomeView.vue'
+import NotFoundViewVue from '@/views/NotFoundView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: HomeViewVue
+    },
+    {
+      name: 'not-found',
+      path: '/:pathMatch(.*)*',
+      component: NotFoundViewVue
+    }
+  ]
 })
 
 export default router
